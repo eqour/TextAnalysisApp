6ae465d321063f1340619e7c31c9c176c4a17eec
Mirrodep <sasha-tuk@mail.ru>
2022-10-05 19:40:33 +0400

    Added analyzers for issue's 7,8.

--- TextAnalysisApp/AnalyzerFactory.cs
+++ TextAnalysisApp/AnalyzerFactory.cs
@@ -16,6 +16,8 @@ namespace TextAnalysisApp.Model
         private static List<IAnalyzer> CreateAnalyzers()
         {
             List<IAnalyzer> analyzers = new List<IAnalyzer>();
+            analyzers.Add(new SentenceCountAnalyzer());
+            analyzers.Add(new AverageSentenceLenghtAnalyzer());
             return analyzers;
         }
     }
