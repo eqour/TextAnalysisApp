6d64c7a34d4d7c42bd2c8f127e24cf76ffb74517
eqour <eqour@yandex.ru>
2022-10-05 13:45:01 +0400

    T-06: add WordsByFirstLetterAnalyzer

--- TextAnalysisApp/AnalyzerFactory.cs
+++ TextAnalysisApp/AnalyzerFactory.cs
@@ -16,6 +16,7 @@ namespace TextAnalysisApp.Model
         private static List<IAnalyzer> CreateAnalyzers()
         {
             List<IAnalyzer> analyzers = new List<IAnalyzer>();
+            analyzers.Add(new WordsByFirstLetterAnalyzer());
             return analyzers;
         }
     }
