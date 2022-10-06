fa20991bcd09dbe4886cd41221a9569910653653
Mirrodep <50985024+Mirrodep@users.noreply.github.com>
2022-10-06 11:04:49 +0400

    T-07: added sentence count analyzer (#14)
    T-07: added sentence count analyzer

---
+++ TextAnalysisApp/Analyzers/SentenceCountAnalyzer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TextAnalysisApp.Model;
+using TextAnalysisApp.Utils;
+
+namespace TextAnalysisApp.Analyzers
+{
+    internal class SentenceCountAnalyzer : IAnalyzer
+    {
+        public string Name => "Подсчёт предложений в тексте.";
+
+        public List<string> Parameters => new List<string>();
+
+        public AnalysisResult Analyze(string text, List<string> parameters)
+        {
+            int count = TextParser.ParseIntoSentences(text).Count;
+            return new AnalysisResult($"Количество предложений в тексте.", count.ToString());
+        }
+    }
+}
