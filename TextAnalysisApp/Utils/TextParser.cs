4c5f499504883a4b440686e8fc4cdc70709c8191
vudgu student <vudgustudent@yandex.ru>
2022-10-08 11:04:08 +0400

    T-04 Реализовала алгоритм выбора знаов препинания

--- TextAnalysisApp/Utils/TextParser.cs
+++ TextAnalysisApp/Utils/TextParser.cs
@@ -9,6 +9,7 @@ namespace TextAnalysisApp.Utils
 {
     public class TextParser
     {
+        private static readonly char[] voidSeparators = new char[] { ' ', '\t', '\n', '\r' };
         public static List<string> ParseIntoWords(string text)
         {
             // todo: задача #2
@@ -23,8 +24,63 @@ namespace TextAnalysisApp.Utils
         
         public static List<string> ParseIntoPunctuationMarks(string text)
         {
-            // todo: задача #4
-            throw new NotImplementedException();
+            return Parse(text, (t, i) => char.IsPunctuation(t, i) && !IsHyphen(t, i));
+        }
+        private static List<string> Parse(string text, Func<string, int, bool> splitSubstringsPredicate)
+        {
+            List<string> ans = new List<string>();
+            foreach (string fragment in SplitTextWithVoids(text))
+                ans.AddRange(SelectSubstrings(fragment, splitSubstringsPredicate));
+            return ans;
+        }
+
+        private static List<string> SplitTextWithVoids(string text)
+        {
+            return new List<string>(text.Split(voidSeparators));
+        }
+
+        private static List<string> SelectSubstrings(string text, Func<string, int, bool> predicate)
+        {
+            int start = -1, end = 0;
+            List<string> ans = new List<string>();
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (start == -1)
+                {
+                    if (predicate.Invoke(text, i))
+                    {
+                        start = end = i;
+                    }
+                }
+                if (start != -1)
+                {
+                    if (predicate.Invoke(text, i))
+                    {
+                        end = i;
+                        if (i == text.Length - 1)
+                        {
+                            ans.Add(text.Substring(start));
+                        }
+                    }
+                    else
+                    {
+                        ans.Add(text.Substring(start, end - start + 1));
+                        start = -1;
+                    }
+                }
+            }
+            return ans;
+        }
+
+        private static bool IsHyphen(string textWithoutSpaces, int indexOfDash)
+        {
+            string t = textWithoutSpaces;
+            int i = indexOfDash;
+            return i > 0
+                && i < t.Length - 1
+                && t[i] == '-'
+                && char.IsLetterOrDigit(t, i - 1)
+                && char.IsLetterOrDigit(t, i + 1);
         }
     }
 }
