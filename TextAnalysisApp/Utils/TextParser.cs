aa61eeb9addcd5c6c104ad551c41cadfe03ae4aa
room22_07122020 <room22_07122020@outlook.com>
2022-10-10 16:42:52 +0400

    Т-02 Реализовала алгоритм выбора слов из текста

--- TextAnalysisApp/Utils/TextParser.cs
+++ TextAnalysisApp/Utils/TextParser.cs
@@ -10,19 +10,27 @@ namespace TextAnalysisApp.Utils
     {
         public static List<string> ParseIntoWords(string text)
         {
-            
-            String s = "На морских берегах я сижу, не в пространное море гляжу, но на небо глаза возвожу.";
-
-            s = new string(s.Where(c => !char.IsPunctuation(c)).ToArray());
-
-            Console.WriteLine(s);
-
             List<string> parts = new List<string>();
-
-            parts.Add("На морских берегах я сижу, не в пространное море гляжу, но на небо глаза возвожу.");
-            return(parts);
-
-
+            if (text.Length == 0)
+                return parts;
+            string r = text[0].ToString();
+            for (int i = 1; i < text.Length - 1; i++)
+            {
+                char c;
+                if (text[i] == '-' && char.IsLetter(text[i - 1]) && char.IsLetter(text[i + 1])) c = '_';
+                else c = text[i];
+                r += c;
+            }
+            r += text[text.Length - 1];
+            string[] split = r.Split(' ', '!', ',', '?', '.', ':', '-', '\'');
+            foreach (string s in split)
+            {
+                if (s != "")
+                {
+                    parts.Add(s.Replace('_', '-'));
+                }
+            }
+            return (parts);
         }
 
         public static List<string> ParseIntoSentences(string text)
